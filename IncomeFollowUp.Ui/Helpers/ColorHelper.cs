5d1ffb758de291a4082c1231868fc23854e5f60b
ImToon <gservais98@gmail.com>
2024-11-16 23:09:17 +0100

    Improve phone UI

---
+++ IncomeFollowUp.Ui/Helpers/ColorHelper.cs
@@ -0,0 +1,10 @@
+using MudBlazor;
+
+namespace IncomeFollowUp.Ui.Helpers;
+
+public static class ColorHelper
+{
+    public static string Green => Colors.Green.Darken2;
+    public static string Red => Colors.Red.Darken2;
+    public static string Gray => Colors.Gray.Lighten5;
+}
